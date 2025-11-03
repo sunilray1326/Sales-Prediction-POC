a44288214ed88694bf4b0e7ea17748f3d826b348
Sunil Ray <sunilray@gmail.com>
2025-11-04 03:20:39 +0530

    removing not needed files

--- Azure SDK Approach/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Navigate to the application directory
-cd /home/site/wwwroot
-
-# Run Streamlit app
-python -m streamlit run streamlit_app.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
-
